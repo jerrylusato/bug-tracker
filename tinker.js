7ff9330bd749952bf8aced387a8ac94a246c5b35
Michael <michael53161@gmail.com>
2021-11-20 13:12:17 +0900

    make some changes

--- tinker.js
+++ tinker.js
@@ -1,8 +1,26 @@
-try {
-    throw new Error("Something went wrong");
-} catch(err) {
-    // console.log(err.message);
-    console.log(err.stack);
-}
+var nodemailer = require('nodemailer');
+const { reportError } = require('wadudu')
 
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mjthecoder@gmail.com',
+    pass: 'Gambia10@@N'
+  }
+});
+
+var mailOptions = {
+  from: 'mjthecoder@gmail.com',
+  to: 'michael53161@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    reportError({ message: error.message, error: error.stack });
+  } else {
+    console.log('Email sent: ' + info.response)
+  }
+});
